4a34ba1f14e55c3dfb9c1d735998cfc3c69d43d0
KilianCallebaut <kilian.callebaut@gmail.com>
2019-03-01 15:31:58 +0100

    Copied

--- idea/src/org/jetbrains/kotlin/idea/inspections/ReplaceManualRangeWithIndecesCalls.kt
+++ idea/src/org/jetbrains/kotlin/idea/inspections/ReplaceManualRangeWithIndecesCalls.kt
@@ -5,11 +5,43 @@
 
 package org.jetbrains.kotlin.idea.inspections
 
-import com.intellij.psi.PsiElement
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElementVisitor
+import org.jetbrains.kotlin.idea.inspections.collections.isCalling
+import org.jetbrains.kotlin.name.FqName
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.psi.callExpressionVisitor
 
 class ReplaceManualRangeWithIndecesCalls : AbstractKotlinInspection() {
-    override fun buildVisitor(holder: Problemsholder, isOnTheFly: Boolean): PsiElementVisitor {
-
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+        return callExpressionVisitor { callExpression ->
+            if (callExpression.isCount()) {
+                holder.registerProblem(
+                    callExpression,
+                    "Could be replaced with `size`",
+                    ReplaceCollectionCountWithSizeQuickFix()
+                )
+            }
+        }
     }
+}
+
+class ReplaceCollectionCountWithSizeQuickFix : LocalQuickFix {
+    override fun getName() = "Replace 'count' with 'size'"
+
+    override fun getFamilyName() = name
+
+    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+        val element = descriptor.psiElement as KtCallExpression
+        element.replace(KtPsiFactory(element).createExpression("size"))
+    }
+}
+
+private fun KtCallExpression.isCount(): Boolean {
+    return this.valueArguments.isEmpty() && isCalling(FqName("kotlin.collections.size"))
 }
