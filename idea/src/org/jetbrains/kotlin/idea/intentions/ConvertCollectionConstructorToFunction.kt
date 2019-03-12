14c6b142ffb564b8b9db2a420f66a7fab902f831
Dereck Bridie <me@devbridie.com>
2019-03-12 10:15:20 +0100

    Convert to KtCallExpression converter

--- idea/src/org/jetbrains/kotlin/idea/intentions/ConvertCollectionConstructorToFunction.kt
+++ idea/src/org/jetbrains/kotlin/idea/intentions/ConvertCollectionConstructorToFunction.kt
@@ -6,30 +6,32 @@
 package org.jetbrains.kotlin.idea.intentions
 
 import com.intellij.openapi.editor.Editor
-import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+import org.jetbrains.kotlin.idea.caches.resolve.resolveToCall
+import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 
-class ConvertCollectionConstructorToFunction : SelfTargetingIntention<KtNameReferenceExpression>(
-    KtNameReferenceExpression::class.java, "Convert Java collection call to function call"
+class ConvertCollectionConstructorToFunction : SelfTargetingIntention<KtCallExpression>(
+    KtCallExpression::class.java, "Convert Java collection call to function call"
 ) {
 
     private val functionMap = hashMapOf(
-        "ArrayList" to "arrayListOf",
-        "HashMap" to "hashMapOf",
-        "HashSet" to "hashSetOf",
-        "LinkedHashMap" to "linkedMapOf",
-        "LinkedHashSet" to "linkedSetOf"
+        "java.util.ArrayList.<init>" to "arrayListOf",
+        "java.util.HashMap.<init>" to "hashMapOf",
+        "java.util.HashSet.<init>" to "hashSetOf",
+        "java.util.LinkedHashMap.<init>" to "linkedMapOf",
+        "java.util.LinkedHashSet.<init>" to "linkedSetOf"
     )
 
-    override fun isApplicableTo(element: KtNameReferenceExpression, caretOffset: Int): Boolean {
-        val name = element.text ?: return false
-        return functionMap.containsKey(name)
+    override fun isApplicableTo(element: KtCallExpression, caretOffset: Int): Boolean {
+        val fq = element.resolveToCall()?.resultingDescriptor?.fqNameSafe?.asString() ?: return false
+        return functionMap.containsKey(fq)
     }
 
-    override fun applyTo(element: KtNameReferenceExpression, editor: Editor?) {
-        val name = element.text ?: return
-        val functionName = functionMap[name] ?: return
-        val arrayListOfCall = KtPsiFactory(element).createIdentifier(functionName)
-        element.replace(arrayListOfCall)
+    override fun applyTo(element: KtCallExpression, editor: Editor?) {
+        val fq = element.resolveToCall()?.resultingDescriptor?.fqNameSafe?.asString() ?: return
+        val toCall = functionMap[fq] ?: return
+        val arrayListOfCall = KtPsiFactory(element).createIdentifier(toCall)
+        element.calleeExpression?.replace(arrayListOfCall)
     }
 }
